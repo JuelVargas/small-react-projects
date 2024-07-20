8890cb7661fb3eb3d4b3b23d9b8909bab9235703
Juel-Vargas <juelvargasrojas@gmail.com>
2024-07-20 17:53:33 -0400

    add: image slider component and sltyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 
 import './App.css'
+import ImageSlider from './Components/image-slider/index.jsx'
 // import Accordion from './Components/accordion/Accordion.jsx'
 import RandomColor from './Components/random-color/index.jsx'
 import StarRating from './Components/star-rating/index.jsx'
 
 function App() {
-  
+
 
   return (
     <>
-    {/* acordion component */}
+      {/* acordion component */}
       {/* <Accordion /> */}
-    {/* random color component */}
-    {/* <RandomColor/> */}
+      {/* random color component */}
+      {/* <RandomColor/> */}
 
-    <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* image slider component */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'} />
 
     </>
   )
