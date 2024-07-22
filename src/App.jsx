5ddefddb0c4e7e057c16f9bcc48cfd8f00fc52c7
Juel-Vargas <juelvargasrojas@gmail.com>
2024-07-22 17:25:50 -0400

    add: load-more-data component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import ImageSlider from './Components/image-slider/index.jsx'
+import LoadMoreData from './Components/load-more-data/index.jsx'
 // import Accordion from './Components/accordion/Accordion.jsx'
 import RandomColor from './Components/random-color/index.jsx'
 import StarRating from './Components/star-rating/index.jsx'
@@ -18,7 +19,10 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* image slider component */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'} />
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'} /> */}
+
+      {/* Load more products component */}
+      <LoadMoreData/>
 
     </>
   )
